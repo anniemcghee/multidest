781c3baf05041d79476175715844813468c1c384
Annie McGhee <anmcghee@expedia.com>
2015-12-01 11:16:16 -0800

    Angular functioning

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var multidestApp = angular.module('MultidestApp', ['ui.bootstrap']);
 
 multidestApp.run(function(){
-  console.log('The app has started.');
+  console.log("The app has started.");
 });
