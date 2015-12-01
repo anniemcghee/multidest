2fef4b83754662c65edfbd0ae9d295da407d1f38
Annie McGhee <anmcghee@expedia.com>
2015-12-01 10:23:43 -0800

    file structure with js loading

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/views'));
+app.use('/', express.static(__dirname));
 app.listen(3000, function() { console.log('Listening at port 3000')});
