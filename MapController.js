1c62378df6d8ae105a66a489237bed729280e399
hlpetway <hlpetway@gmail.com>
2015-12-01 12:51:21 -0800

    markers are loading onto map! Woo whoo!

--- MapController.js
+++ MapController.js
@@ -12,79 +12,84 @@ var airports = [{
 "name": "Sea Tac International Aiport",
 "city": "Seattle",
 "airportCode": "SEA",
-"coordinates": [
-  47.4502, -122.3438
-]}, {
+"lat": 47.4502,
+"long": -122.3438
+}, {
 "name": "Portland International Aiport",
 "city": "Portland",
 "airportCode": "PDX",
-"coordinates": [
-  45.5897, -122.5972
-]}, {
+"lat": 45.5897,
+"long": -122.5972
+}, {
 "name": "San Francisco International Aiport",
 "city": "San Francisco",
 "airportCode": "SFO",
-"coordinates": [
-  37.6213, -122.3811
-]}, {
+"lat": 37.6213,
+"long": -122.3811
+}, {
 "name": "Los Angeles International Aiport",
 "city": "Los Angeles",
 "airportCode": "LAX",
-"coordinates": [
-  33.9415, -118.4107
-]}, {
+"lat": 33.9415,
+"long": -118.4107
+}, {
 "name": "San Jose Aiport",
 "city": "San Jose",
 "airportCode": "SJC",
-"coordinates": [
-  37.3639, -121.9311
-]}, {
+"lat": 37.3639,
+"long": -121.9311
+}, {
 "name": "San Diego International Aiport",
 "city": "San Diego",
 "airportCode": "SAN",
-"coordinates": [
-  32.7338, -117.1954
-]}, {
+"lat": 32.7338,
+"long": -117.1954
+}, {
 "name": "JFK International Aiport",
 "city": "New York City",
 "airportCode": "JFK",
-"coordinates": [
-  40.6413, -73.7803
-]}, {
+"lat": 40.6413,
+"long": -73.7803
+}, {
 "name": "La Guardia International Aiport",
 "city": "New York City",
 "airportCode": "LGA",
-"coordinates": [
-  40.7769, -73.8761
-]}, {
+"lat": 40.7769,
+"long": -73.8761
+}, {
 "name": "Logan International Aiport",
 "city": "Boston",
 "airportCode": "BOS",
-"coordinates": [
-  42.3656, -71.0117
-]}, {
+"lat": 42.3656,
+"long": -71.0117
+}, {
 "name": "Dulles International Aiport",
 "city": "Washington, D.C.",
 "airportCode": "IAD",
-"coordinates": [
-  38.9531, -77.4587
-]}, {
+"lat": 38.9531,
+"long": -77.4587
+}, {
 "name": "Ronald Reagan Washington National Aiport",
 "city": "Washington, D.C.",
 "airportCode": "DCA",
-"coordinates": [
-  38.8512, -77.0424
-]}, {
+"lat": 38.8512,
+"long": -77.0424
+}, {
 "name": "Miami International Aiport",
 "city": "Miami",
 "airportCode": "MIA",
-"coordinates": [
-  25.7958, -80.2892
-]}];
+"lat": 25.7958,
+"long":-80.2892
+}];
+
+console.log([airports[0].lat],[airports[0].long]);
+console.log(parseFloat(airports[0].long));
+
+L.marker([50.5, -122.5]).addTo(map);
 
 var loadmarkers = function (airport) {
   for (var i = 0; i < airport.length; i++) {
-    L.marker([airport[i].coordinates[0]],[airport[i].coordinates[1]]).addTo(map);
+    L.marker([airport[i].lat, airport[i].long]).addTo(map);
   }
 };
 
