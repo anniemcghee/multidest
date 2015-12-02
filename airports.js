bdca68a3ccd93e87dd6c466b6d23ee71264f24d1
Annie McGhee <anmcghee@expedia.com>
2015-12-02 10:00:58 -0800

    final commit

--- airports.js
+++ airports.js
@@ -1,101 +1,101 @@
 var airports = [{
-"name": "Sea Tac International Aiport",
+"name": "Sea Tac International Airport",
 "city": "Seattle",
 "airportCode": "SEA",
 "lat": 47.4502,
 "long": -122.3438
 }, {
-"name": "Portland International Aiport",
+"name": "Portland International Airport",
 "city": "Portland",
 "airportCode": "PDX",
 "lat": 45.5897,
 "long": -122.5972
 }, {
-"name": "San Francisco International Aiport",
+"name": "San Francisco International Airport",
 "city": "San Francisco",
 "airportCode": "SFO",
 "lat": 37.6213,
 "long": -122.3811
 }, {
-"name": "Los Angeles International Aiport",
+"name": "Los Angeles International Airport",
 "city": "Los Angeles",
 "airportCode": "LAX",
 "lat": 33.9415,
 "long": -118.4107
 }, {
-"name": "San Jose Aiport",
+"name": "San Jose Airport",
 "city": "San Jose",
 "airportCode": "SJC",
 "lat": 37.3639,
 "long": -121.9311
 }, {
-"name": "San Diego International Aiport",
+"name": "San Diego International Airport",
 "city": "San Diego",
 "airportCode": "SAN",
 "lat": 32.7338,
 "long": -117.1954
 }, {
-"name": "Denver International Aiport",
+"name": "Denver International Airport",
 "city": "Denver",
 "airportCode": "DEN",
 "lat": 39.8560,
 "long": -104.6759
 }, {
-"name": "O'Hare International Aiport",
+"name": "O'Hare International Airport",
 "city": "Chicago",
 "airportCode": "CHI",
 "lat": 41.9741,
 "long": -87.9095
 }, {
-"name": "Salt Lake City International Aiport",
+"name": "Salt Lake City International Airport",
 "city": "Salt Lake City",
 "airportCode": "SLC",
 "lat": 40.7899,
 "long": -111.9812
 }, {
-"name": "Atlanta International Aiport",
+"name": "Atlanta International Airport",
 "city": "Atlanta",
 "airportCode": "ATL",
 "lat": 33.6407,
 "long": -84.4298
 }, {
-"name": "Dallas Fort Worth International Aiport",
+"name": "Dallas Fort Worth International Airport",
 "city": "Dallas",
 "airportCode": "DFW",
 "lat": 32.8998,
 "long": -97.0425
 }, {
-"name": "JFK International Aiport",
+"name": "JFK International Airport",
 "city": "New York City",
 "airportCode": "JFK",
 "lat": 40.6413,
 "long": -73.7803
 }, {
-"name": "La Guardia International Aiport",
+"name": "La Guardia International Airport",
 "city": "New York City",
 "airportCode": "LGA",
 "lat": 40.7769,
 "long": -73.8761
 }, {
-"name": "Logan International Aiport",
+"name": "Logan International Airport",
 "city": "Boston",
 "airportCode": "BOS",
 "lat": 42.3656,
 "long": -71.0117
 }, {
-"name": "Dulles International Aiport",
+"name": "Dulles International Airport",
 "city": "Washington, D.C.",
 "airportCode": "IAD",
 "lat": 38.9531,
 "long": -77.4587
 }, {
-"name": "Ronald Reagan Washington National Aiport",
+"name": "Ronald Reagan Washington National Airport",
 "city": "Washington, D.C.",
 "airportCode": "DCA",
 "lat": 38.8512,
 "long": -77.0424
 }, {
-"name": "Miami International Aiport",
+"name": "Miami International Airport",
 "city": "Miami",
 "airportCode": "MIA",
 "lat": 25.7958,
