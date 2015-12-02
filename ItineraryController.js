fdcf63827221c625cbe56a3f2af1f2cfe413b164
Annie McGhee <anmcghee@expedia.com>
2015-12-01 18:07:17 -0800

    itinerary and marker click functioning

--- ItineraryController.js
+++ ItineraryController.js
@@ -1,5 +1,33 @@
-multidestApp.controller('Itinerary', function ($scope, $http, SelectedAirport) {
+multidestApp.controller('Itinerary', function ($scope, $http, $compile, SelectedAirport) {
   console.log("itinerary controller loading");
+
+  //first load map and bind markers to scope for selection
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYW5uaWVtY2JsZWUiLCJhIjoiV1VYZ09NRSJ9.STqM6FSiQ4WKq_I-hJS1QQ';
+
+  var map = L.mapbox.map('map')
+    .setView([40.50, -100.38], 5)
+    .addLayer(L.mapbox.tileLayer('mapbox.streets'));
+
+  var loadMarkers = function (airport) {
+    for (var i = 0; i < airport.length; i++) {
+
+      var html = '<button class="btn btn-warning" id="' + airport[i].airportCode + '" ng-click="selectedAirport(' + airport[i].airportCode + ')">Add to Trip</button>'
+
+      var marker = L.marker([airport[i].lat, airport[i].long], {title: airport[i].airportCode}).addTo(map).bindPopup(html);
+
+
+      marker.on('click', function(e) {
+        $scope.selectedAirport = e.target.options.title;
+        itinerary.push($scope.selectedAirport);
+        console.log(itinerary);
+        calculateLegTotal(itinerary[0 + counter], itinerary[1 + counter]);
+      });
+    }
+  };
+
+  loadMarkers(airports);
+
+  //then itinerary logic and route creation
   var legTotal;
   var counter = 0;
 
@@ -12,15 +40,6 @@ multidestApp.controller('Itinerary', function ($scope, $http, SelectedAirport) {
   $scope.legTotals = [];
 
   // $scope.legTotal = legTotal;
-
-  var selectedAirport = function(airport) {
-    itinerary.push(airport);
-    $scope.itinerary = itinerary;
-    console.log("ITIN", itinerary);
-    if($scope.itinerary.length > 1) {
-      calculateLegTotal($scope.itinerary[0 + counter], $scope.itinerary[1 + counter]);
-    }
-  }
 
   var calculateLegTotal = function(dep, arr) {
     console.log("CALLING CALC FUNCTION");
